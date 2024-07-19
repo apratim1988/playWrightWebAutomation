7b221cb847f1643544feb428c3ce064a803e2aa6
apratim1988 <apratim.chaudhuri@gmail.com>
2024-07-19 15:37:52 +0530

    updated screen capture logic

--- tests/base_test.py
+++ tests/base_test.py
@@ -12,23 +12,28 @@ class BaseTest:
     def setup_class(cls):
         Config.ensure_directories()
 
-    def clear_screenshot_for_method(self):
-        screenshot_dir = os.path.join(Config.SCREENSHOT_DIR, self.method_name)
+    @classmethod
+    def clear_screenshot_for_method(cls, method_name):
+        screenshot_dir = os.path.join(Config.SCREENSHOT_DIR, method_name)
         if os.path.exists(screenshot_dir):
             shutil.rmtree(screenshot_dir)
 
     def setup_method(self, method):
-        self.method_name = method.__name__
-        self.clear_screenshot_for_method()  # Call to clear existing screenshots
         Config.ensure_directories()
+        self.method_name = method.__name__
+
+        # Only clear the screenshots directory once per method
+        if not hasattr(self, 'method_setup_done'):
+            self.clear_screenshot_for_method(self.method_name)
+            self.__class__.method_setup_done = True
 
     def initialize_logger(self, data_set_id):
         self.data_set_id = data_set_id
         self.logger = setup_logger(self.method_name, self.data_set_id)
 
     def take_screenshot(self, page, step_name, data_set_id):
-        # Delegate to the util function, using method info, and get the path back
-        return capture_screenshot(page, self.method_name, step_name, data_set_id)
+        # Delegate to the util function, using method info
+        capture_screenshot(page, self.method_name, step_name, data_set_id)
 
     def fail_test(self, page, error_message, data_set_id):
         self.logger.error(error_message, exc_info=True)
--- utils/screenshot_utils.py
+++ utils/screenshot_utils.py
@@ -3,16 +3,19 @@ import time
 from utils.config import Config
 
 def capture_screenshot(page, test_case_name, step_name, data_set_id):
+    # Create the base screenshot directory if it doesn't exist
     screenshot_dir = os.path.join(Config.SCREENSHOT_DIR, test_case_name)
     if not os.path.exists(screenshot_dir):
         os.makedirs(screenshot_dir)
 
+    # Create subdirectory for the data set
     data_set_dir = os.path.join(screenshot_dir, f"data_set_{data_set_id}")
     if not os.path.exists(data_set_dir):
         os.makedirs(data_set_dir)
 
+    # Add a timestamp to the screenshot filename to make it unique
     timestamp = time.strftime("%Y%m%d-%H%M%S")
     screenshot_path = os.path.join(data_set_dir, f"{step_name}_{timestamp}.png")
-    page.screenshot(path=screenshot_path)
 
-    return screenshot_path  # Return the path where the screenshot is saved
+    # Capture and save the screenshot
+    page.screenshot(path=screenshot_path)
