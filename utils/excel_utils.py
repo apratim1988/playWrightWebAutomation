60079b86b3350e7a31d4e030bb571ec31f944081
apratim1988 <apratim.chaudhuri@gmail.com>
2024-08-19 11:42:46 +0530

    Added data faking

--- utils/excel_utils.py
+++ utils/excel_utils.py
@@ -1,4 +1,5 @@
 import openpyxl
+from faker import Faker
 
 def read_test_data(file_path, sheet_name):
     workbook = openpyxl.load_workbook(file_path)
@@ -6,7 +7,16 @@ def read_test_data(file_path, sheet_name):
     data = []
 
     headers = [cell.value for cell in sheet[1]]
+    fake = Faker()
+
     for row in sheet.iter_rows(min_row=2, values_only=True):
-        data.append(dict(zip(headers, row)))
+        test_data = dict(zip(headers, row))
+
+        # Add dynamically generated data
+        test_data['first_name'] = fake.first_name()
+        test_data['last_name'] = fake.last_name()
+        test_data['zipcode'] = fake.zipcode()
+
+        data.append(test_data)
 
     return data
